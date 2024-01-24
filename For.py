c0a6637659e14a9eb5bd9c3864132295e1a20077
Rafael Braz Gomes <149207016+RBG04@users.noreply.github.com>
2024-01-24 16:29:21 -0300

    Aprendendo sobre "For"

---
+++ For.py
@@ -0,0 +1,14 @@
+'''
+clubes=['palmeiras','corinthians','fortaleza','santos','flamengo']
+for x in clubes:
+    print(x)
+'''
+'''
+for letra in "rafael":
+    print(letra)
+'''
+'''
+clubes=['palmeiras','corinthians','fortaleza','santos','flamengo']
+for i in range(1,3):
+    print(clubes[i])
+'''
