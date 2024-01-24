4e8781f6775b0d543334033c696fd0a55dd6ee20
Rafael Braz Gomes <rafaelbrazgomes04@gmail.com>
2024-01-24 12:59:04 -0300

    Primeiros Passos - Python

---
+++ Primeiros Passos.py
@@ -0,0 +1,36 @@
+'''print("Hello, World!")'''
+
+'''if 5 > 2:
+  print("Five is greater than two!")'''
+
+'''x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)'''
+
+'''fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)'''
+
+'''
+x = 1
+y = 2.8 
+z = 1j 
+
+a = float(x)
+b = int(y)
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))'''
+
+'''import random
+print(random.randrange(1, 10))'''
+
